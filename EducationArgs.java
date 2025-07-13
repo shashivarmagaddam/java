b9dfaa27e74ff372eee5e6b2e6a5285049459179
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-13 23:27:22 +0530

    Create EducationArgs.java
    Education Args

---
+++ EducationArgs.java
@@ -0,0 +1,10 @@
+class EducationArgs{
+ public static void main(String[] args){
+  System.out.println("College Name : ");
+  System.out.println(arg[0]);
+  System.out.println("Branch : ");
+  System.out.println(args[1]);
+  System.out.println("CGPA : ");
+  System.out.println(args[2]);
+}
+}
