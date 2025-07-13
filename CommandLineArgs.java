09cc8a11c720845c0d6dac4295bf1151dbc0cdd1
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-13 23:20:10 +0530

    CommandLineArgs.java
    This Contains Command Line Arguments.

---
+++ CommandLineArgs.java
@@ -0,0 +1,10 @@
+class CommandLineArgs{
+  public static void main(String[] args){
+    System.out.print("Name : ");
+    System.out.println(args[0]);
+    System.out.print("Age : ");
+    System.out.print(args[1]);
+    System.out.print("Gender : ");
+    System.out.print("args[2]);
+  }
+}
