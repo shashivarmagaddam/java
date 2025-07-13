a92c43c1aec9d9559817123c40a671a864619efa
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-13 23:33:02 +0530

    Create FamilyDetails.java
    Family Details Args

---
+++ FamilyDetails.java
@@ -0,0 +1,10 @@
+class Family{
+  public static void main(String[] args)[
+  System.out.print("Father Name : ");
+  System.out.println(args[0]);
+  System.out.print("Mother Name : "):
+  System.out.println(args[1]);
+  System.out.print("Brother or Sister Name : ");
+  System.out.println(args[2]);
+}
+}
