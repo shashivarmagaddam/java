38f02461eaeb735d016dc1223b57c2a3cf598f5c
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-25 17:19:21 +0530

    Create RaviVisa.java

---
+++ RaviVisa.java
@@ -0,0 +1,11 @@
+class RaviVisa{
+public static void main(String[] args){
+int age=25;
+if(age>=18)
+System.out.println("Passport is valid passport,application is Accepted");
+else if(age<18)
+System.out.println("No passport,the application is rejected");
+else 
+System.out.println("Visa is not allowed");
+}
+}
