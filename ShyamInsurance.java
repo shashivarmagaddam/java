b55e24c54236fa3eb82a3a8cbe65be7f2668f71d
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-25 17:32:55 +0530

    Add files via upload

---
+++ ShyamInsurance.java
@@ -0,0 +1,17 @@
+public class ShyamInsurance {
+ public static void main(String[] args) {
+  int age = 55;
+        if (age > 60) {
+            System.out.println("Premium: ₹5000");
+        } else if (age >= 40) {
+            System.out.println("Premium: ₹3000");
+        } else if (age >= 20) {
+            System.out.println("Premium: ₹1500");
+        } else {
+            System.out.println("Premium: ₹1000");
+        }
+
+
+ }
+
+}
